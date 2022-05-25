219466f5785ed08ab3e2ef299228e68a13403bfe
EC2 Default User <ec2-user@ip-172-31-33-31.ec2.internal>
2022-05-25 01:09:53 +0000

    [Update]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new #16
   end
 
   def create
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,9 +17,9 @@ class UsersController < ApplicationController
 
   def update
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to user_path, notice: "You have updated user successfully." #14
     else
-      render "show"
+      render :edit #15
     end
   end
 
